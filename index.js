ee340c0cadece191658686ddfb4d42b13e7f71b2
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 15:40:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ class Driver {
   yearsExperienceFromBeginningOf(year) {
     return year - this.startDate.getFullYear()
   }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
 }
