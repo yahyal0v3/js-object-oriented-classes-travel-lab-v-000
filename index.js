dca0f248bbc4a8c2185b9eebc3e0cf0aa0fab4e4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 16:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const horizontalDistance = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal)
+    const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return horizontalDistance + verticalDistance
+  }
+
+  estimatedTime(peak) {
+    let time = (peak === true) ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3
+    return time
+  }
+}
